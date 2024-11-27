3c2b25c31d7383b961e4e968454e053d5979ad71
Waleed Lugod <lugodwaleed@gmail.com>
2024-11-27 19:00:35 +0800

    create all tables for setup

--- backend/setup.sql
+++ backend/setup.sql
@@ -1,11 +1,6 @@
 DROP DATABASE tt;
 CREATE DATABASE tt;
 USE tt;
-CREATE TABLE test(
-    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-INSERT INTO test(name) VALUES("tt");
 
 CREATE TABLE TRAIN(
     train_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
@@ -32,19 +27,79 @@ CREATE TABLE WORKER(
     middle_initial VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     crew_id INT NOT NULL,
-    FOREIGN KEY (crew_id) REFERENCES CREW(crew_id) ON DELETE RESTRICT
+    FOREIGN KEY (crew_id) REFERENCES CREW(crew_id)
 );
 
 ALTER TABLE CREW ADD CONSTRAINT FK_CREW_WORKER
-    FOREIGN KEY (manager_id) REFERENCES WORKER(worker_id) ON DELETE RESTRICT;
+    FOREIGN KEY (manager_id) REFERENCES WORKER(worker_id);
 
 CREATE TABLE MAINTENANCE_LOG(
     maintenance_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    log_date DATE NOT NULL,
+    log_date DATETIME NOT NULL,
     task VARCHAR(255) NOT NULL,
-    cond VARCHAR(255) NOT NULL,
+    cond VARCHAR(255) NOT NULL, -- bad, ok, very good, excellent
     train_id INT NOT NULL,
     crew_id INT NOT NULL,
-    FOREIGN KEY (train_id) REFERENCES TRAIN(train_id) ON DELETE RESTRICT,
-    FOREIGN KEY (crew_id) REFERENCES CREW(crew_id) ON DELETE RESTRICT
+    FOREIGN KEY (train_id) REFERENCES TRAIN(train_id),
+    FOREIGN KEY (crew_id) REFERENCES CREW(crew_id),
+	CONSTRAINT CK_cond CHECK (cond IN ('bad', 'ok', 'very good', 'excellent'))
+);
+
+CREATE TABLE PASSENGER(
+    passenger_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    given_name VARCHAR(255) NOT NULL,
+    middle_initial VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+	birth_date DATETIME NOT NULL,
+	gender VARCHAR(6) NOT NULL, -- male, female
+	CONSTRAINT CK_gender CHECK (gender IN ('male', 'female'))
+);
+
+CREATE TABLE TICKET(
+    ticket_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	total_cost INT NOT NULL,
+	date_purchased DATETIME NOT NULL,
+	passenger_id INT NOT NULL,
+	FOREIGN KEY (passenger_id) REFERENCES PASSENGER(passenger_id)
+);
+
+CREATE TABLE TRIP_SCHEDULE(
+    schedule_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	schedule_date DATE NOT NULL
+);
+
+CREATE TABLE STATION(
+    station_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	station_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE ROUTE(
+    route_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	travel_time TIME NOT NULL,
+	system_type VARCHAR(9), -- local, intertown
+	origin_station_id INT NOT NULL,
+	destination_station_id INT NOT NULL,
+	FOREIGN KEY (origin_station_id) REFERENCES STATION(station_id),
+	FOREIGN KEY (destination_station_id) REFERENCES STATION(station_id),
+	CONSTRAINT CK_system_type CHECK (system_type IN ('local', 'intertown'))
+);
+
+CREATE TABLE TRIP(
+    trip_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	departure DATETIME NOT NULL,
+	arrival DATETIME NOT NULL,
+	train_id INT NOT NULL,
+	route_id INT NOT NULL,
+	schedule_id INT NOT NULL,
+	FOREIGN KEY (train_id) REFERENCES TRAIN(train_id),
+	FOREIGN KEY (route_id) REFERENCES ROUTE(route_id),
+	FOREIGN KEY (schedule_id) REFERENCES TRIP_SCHEDULE(schedule_id)
+);
+
+CREATE TABLE TICKET_TRIP(
+    ticket_trip_id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+	ticket_id INT NOT NULL,
+	trip_id INT NOT NULL,
+	FOREIGN KEY (ticket_id) REFERENCES TICKET(ticket_id),
+	FOREIGN KEY (trip_id) REFERENCES TRIP(trip_id)
 );
