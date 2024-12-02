dd5779715e4bb09d3612e6fc5ca871bcf6f90108
Waleed Lugod <lugodwaleed@gmail.com>
2024-12-02 19:59:37 +0800

    implement insert

--- backend/server.js
+++ backend/server.js
@@ -1,148 +1,166 @@
-import 'dotenv/config'
+import "dotenv/config";
 
-import express from 'express'
-const app = express()
-app.listen(8080)
+import express from "express";
+const app = express();
+app.listen(8080);
 
-import mysql from 'mysql'
+import mysql from "mysql";
 export const db = mysql.createConnection({
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
-  database: 'tt'
-})
+  database: "tt",
+});
 
-db.connect()
+db.connect();
 
 // serve files in frontend folder by default
-app.use(express.static('frontend'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static("frontend"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/test', (req, res) => {
-  const data = {test: 'this is some data'}
-	res.send(data)
-})
+app.get("/api/test", (req, res) => {
+  const data = { test: "this is some data" };
+  res.send(data);
+});
 
-app.get('/api/db_test', (req, res) => {
-  db.query('SELECT * FROM test', (error, results, fields) => {
-    res.send(results)
-    console.log(error)
-    console.log(results)
-    console.log(fields)
-  })
-})
+app.get("/api/db_test", (req, res) => {
+  db.query("SELECT * FROM test", (error, results, fields) => {
+    res.send(results);
+    console.log(error);
+    console.log(results);
+    console.log(fields);
+  });
+});
 
-app.get('/api/logs', (req, res) => {
-  let crewNameConcat = "CONCAT(last_name, ', ',initial, '.')"
+app.get("/api/logs", (req, res) => {
+  let crewNameConcat = "CONCAT(last_name, ', ',initial, '.')";
 
-  let sQuery1 = "SELECT maintenance_id 'Maintenance ID', log_date 'Log Date', task 'Task', "
-  let sQuery2 = `cond 'Condition', train_id 'Train ID',${crewNameConcat} 'Crew Name' `
-  let jQurery = "FROM MAINTENANCE_LOG ml JOIN CREW c ON ml.crew_id = c.crew_id "
+  let sQuery1 =
+    "SELECT maintenance_id 'Maintenance ID', log_date 'Log Date', task 'Task', ";
+  let sQuery2 = `cond 'Condition', train_id 'Train ID',${crewNameConcat} 'Crew Name' `;
+  let jQurery =
+    "FROM MAINTENANCE_LOG ml JOIN CREW c ON ml.crew_id = c.crew_id ";
 
-  let query = sQuery1 + sQuery2 + jQurery
+  let query = sQuery1 + sQuery2 + jQurery;
   const filters = [];
 
   for (const param in req.query) {
-    if (param == 'date'){
-      filters.push(`log_date BETWEEN ${db.escape(req.query[param] + ' 00:00:00')}` + 
-        ` AND ${db.escape(req.query[param] + ' 23:59:59')} `)
-    }else if (param == 'crew-name'){
-      filters.push(`${crewNameConcat} LIKE ${db.escape('%'+req.query[param]+'%')}`)
-    }else
-      filters.push(`${param} = ${db.escape(req.query[param])}`);
+    if (param == "date") {
+      filters.push(
+        `log_date BETWEEN ${db.escape(req.query[param] + " 00:00:00")}` +
+          ` AND ${db.escape(req.query[param] + " 23:59:59")} `,
+      );
+    } else if (param == "crew-name") {
+      filters.push(
+        `${crewNameConcat} LIKE ${db.escape("%" + req.query[param] + "%")}`,
+      );
+    } else filters.push(`${param} = ${db.escape(req.query[param])}`);
   }
-  console.log(filters)
+  console.log(filters);
   if (filters.length > 0) {
     query += " WHERE " + filters.join(" AND ");
   }
-  
-  console.log(query)
+
+  console.log(query);
   db.query(query, (error, results, fields) => {
-    res.send(results)
-    console.log(error)
-    console.log(results)
-    console.log(fields)
-  })
-	console.log(req.method)
-})
+    res.send(results);
+    console.log(error);
+    console.log(results);
+    console.log(fields);
+  });
+});
 
+app.post("/api/logs", (req, res) => {
+	db.query(`INSERT INTO MAINTENANCE_LOG (log_date, task, cond, train_id, crew_id) VALUES
+		('${req.body.date} 00:00:00', "test", "${req.body.cond}", 1, 1)`, (error, result) => {
+			res.send(result)
+			console.log(error)
+			console.log(result)
+		})
+  // db.query(
+  //   `
+		// INSERT INTO MAINTENANCE_LOG (log_date, task, cond, train_id, crew_id) VALUES
+		// ('${req.body.date}', 'task test', '${req.body.cond}', 1, 1);`,
+  //   (error, results, field) => {
+  //     res.sendStatus(201).send(result);
+  //     console.log(error);
+  //     console.log(results);
+  //     console.log(fields);
+  //   },
+  // );
+});
 
-app.post('/api/logs', (req, res) => {
-	console.log(req.body)
-	res.send(200)
-})
+app.get("/api/stations", (req, res) => {
+  let query =
+    "SELECT station_id 'Station ID', station_name 'Station Name' FROM STATION";
 
-app.get('/api/stations', (req, res) => {
-
-  let query = "SELECT station_id 'Station ID', station_name 'Station Name' FROM STATION"
-  
-  console.log(query)
+  console.log(query);
   db.query(query, (error, results, fields) => {
-    res.send(results)
-    console.log(error)
-    console.log(results)
-    console.log(fields)
-  })
-})
+    res.send(results);
+    console.log(error);
+    console.log(results);
+    console.log(fields);
+  });
+});
 
+app.post("/api/stations", (req, res) => {
+  console.log(req.body);
+  res.send(200);
+});
 
-app.post('/api/stations', (req, res) => {
-	console.log(req.body)
-	res.send(200)
-})
-
-app.get('/api/destinationRoutes', (req, res) => {
+app.get("/api/destinationRoutes", (req, res) => {
   console.log(req.query);
   const destination = req.query.destination;
   console.log(destination);
-  let sQuery1 = "SELECT r.route_id 'Route ID', st.station_name 'Origin Station', s.station_name 'Destination Station' FROM ROUTE r , STATION s, STATION st WHERE r.destination_station_id = s.station_id AND st.station_id = r.origin_station_id AND r.destination_station_id = ";
+  let sQuery1 =
+    "SELECT r.route_id 'Route ID', st.station_name 'Origin Station', s.station_name 'Destination Station' FROM ROUTE r , STATION s, STATION st WHERE r.destination_station_id = s.station_id AND st.station_id = r.origin_station_id AND r.destination_station_id = ";
   sQuery1 += destination;
 
-  let query = sQuery1
+  let query = sQuery1;
   const filters = [];
-  
-  console.log(query)
+
+  console.log(query);
   db.query(query, (error, results, fields) => {
-    res.send(results)
-    console.log(error)
-    console.log(results)
-    console.log(fields)
-  })
-})
+    res.send(results);
+    console.log(error);
+    console.log(results);
+    console.log(fields);
+  });
+});
 
-
-app.get('/api/outgoingRoutes', (req, res) => {
+app.get("/api/outgoingRoutes", (req, res) => {
   const origin = req.query.origin;
-  let sQuery1 = "SELECT r.route_id 'Route ID', s.station_name 'Origin Station', st.station_name 'Destination Station' FROM ROUTE r , STATION s, STATION st WHERE r.origin_station_id = s.station_id AND st.station_id = r.destination_station_id AND r.origin_station_id =  ";
-  sQuery1 += origin ;
+  let sQuery1 =
+    "SELECT r.route_id 'Route ID', s.station_name 'Origin Station', st.station_name 'Destination Station' FROM ROUTE r , STATION s, STATION st WHERE r.origin_station_id = s.station_id AND st.station_id = r.destination_station_id AND r.origin_station_id =  ";
+  sQuery1 += origin;
 
-  let query = sQuery1
+  let query = sQuery1;
   const filters = [];
-  
-  db.query(query, (error, results, fields) => {
-    res.send(results)
-    console.log(error)
-    console.log(results)
-    console.log(fields)
-  })
-})
 
-app.get('/api/tickets', (req, res) => {
+  db.query(query, (error, results, fields) => {
+    res.send(results);
+    console.log(error);
+    console.log(results);
+    console.log(fields);
+  });
+});
+
+app.get("/api/tickets", (req, res) => {
   const name = req.query.passenger;
 
-  let passengerNameMacro = `CONCAT(given_name, ' ', middle_initial, '. ', last_name)`
+  let passengerNameMacro = `CONCAT(given_name, ' ', middle_initial, '. ', last_name)`;
 
-  let sQuery1 = `SELECT t.ticket_id, t.total_cost, t.date_purchased, p.passenger_id, `
-  let sQuery2 = `tr.departure, tr.arrival, tr.cost, ${passengerNameMacro} 'Passenger Name' `
-  let fQuery = `FROM TICKET t, TICKET_TRIP tt, TRIP tr, PASSENGER p `
-  let wQuery = `WHERE t.ticket_id = tt.ticket_id AND tr.trip_id = tt.trip_id AND p.passenger_id = t.passenger_id `
-  let nQuery = `AND ${passengerNameMacro} LIKE ${db.escape('%'+name+'%')}`
+  let sQuery1 = `SELECT t.ticket_id, t.total_cost, t.date_purchased, p.passenger_id, `;
+  let sQuery2 = `tr.departure, tr.arrival, tr.cost, ${passengerNameMacro} 'Passenger Name' `;
+  let fQuery = `FROM TICKET t, TICKET_TRIP tt, TRIP tr, PASSENGER p `;
+  let wQuery = `WHERE t.ticket_id = tt.ticket_id AND tr.trip_id = tt.trip_id AND p.passenger_id = t.passenger_id `;
+  let nQuery = `AND ${passengerNameMacro} LIKE ${db.escape("%" + name + "%")}`;
 
   let query = sQuery1 + sQuery2 + fQuery + wQuery + nQuery;
   db.query(query, (error, results, fields) => {
-    res.send(results)
-    console.log(error)
-    console.log(results)
-    console.log(fields)
-  })
-})
+    res.send(results);
+    console.log(error);
+    console.log(results);
+    console.log(fields);
+  });
+});
--- frontend/main.js
+++ frontend/main.js
@@ -1,63 +1,67 @@
 function getMaintenanceLog() {
-  const form = document.getElementById('maintenance-log-filter');
+  const form = document.getElementById("maintenance-log-filter");
   const formData = new FormData(form);
 
-  const filters = {}
+  const filters = {};
   for (let [key, value] of formData.entries()) {
-    if (value !== '') {
-      filters[key] = value
+    if (value !== "") {
+      filters[key] = value;
     }
   }
-  console.log(filters)
+  console.log(filters);
 
   const queryParams = new URLSearchParams(filters).toString();
   console.log(queryParams);
   fetch(`/api/logs?${queryParams}`)
-    .then(res => res.json())
-    .then((res) =>{
+    .then((res) => res.json())
+    .then((res) => {
       const app = document.getElementById("maintenance");
-      app.innerHTML=''
+      app.innerHTML = "";
 
-      const logTable = document.createElement('ul')
-      for (const maintenanceLog of res){
-        const logElement = document.createElement('li')
-        logElement.append('Maintenance Log #: ' + maintenanceLog['Maintenance ID'])
-        const logCols = document.createElement('ul')
-        logElement.append(logCols)
+      const logTable = document.createElement("ul");
+      for (const maintenanceLog of res) {
+        const logElement = document.createElement("li");
+        logElement.append(
+          "Maintenance Log #: " + maintenanceLog["Maintenance ID"],
+        );
+        const logCols = document.createElement("ul");
+        logElement.append(logCols);
 
-        for (const col in maintenanceLog){
-          if (col == 'Maintenance ID') continue
-          const logCol = document.createElement('li')
-          const elem = col + ': ' + maintenanceLog[col]
-          logCol.append(elem)
-          logCols.append(logCol)
+        for (const col in maintenanceLog) {
+          if (col == "Maintenance ID") continue;
+          const logCol = document.createElement("li");
+          const elem = col + ": " + maintenanceLog[col];
+          logCol.append(elem);
+          logCols.append(logCol);
         }
-        logTable.append(logElement)
+        logTable.append(logElement);
       }
-      app.append(logTable)
-    })
+      app.append(logTable);
+    });
 }
 
 function getStations() {
-  fetch('/api/stations')
-    .then(res => res.json())
+  fetch("/api/stations")
+    .then((res) => res.json())
     .then((res) => {
       const app = document.getElementById("station");
-      app.innerHTML = '';
+      app.innerHTML = "";
 
-      const stationTable = document.createElement('ul');
+      const stationTable = document.createElement("ul");
       for (const station of res) {
-        const stationElement = document.createElement('li');
-        const stationCols = document.createElement('ul');
+        const stationElement = document.createElement("li");
+        const stationCols = document.createElement("ul");
         stationElement.append(stationCols);
 
-        for (const col in station) { 
-          const stationCol = document.createElement('li');
+        for (const col in station) {
+          const stationCol = document.createElement("li");
           const elem = `${col}: ${station[col]}`;
           stationCol.append(elem);
           stationCols.append(stationCol);
-        }stationTable.append(stationElement);
-      }app.append(stationTable);
+        }
+        stationTable.append(stationElement);
+      }
+      app.append(stationTable);
     });
 }
 
@@ -66,24 +70,26 @@ function getDestinationRoutes() {
   const destQuery = `destination=${destination}`;
 
   fetch(`/api/destinationRoutes?${destQuery}`)
-    .then(res => res.json())
+    .then((res) => res.json())
     .then((res) => {
       const app = document.getElementById("station");
-      app.innerHTML = '';
+      app.innerHTML = "";
 
-      const routesTable = document.createElement('ul');
+      const routesTable = document.createElement("ul");
       for (const route of res) {
-        const routeElement = document.createElement('li');
-        const routeCols = document.createElement('ul');
+        const routeElement = document.createElement("li");
+        const routeCols = document.createElement("ul");
         routeElement.append(routeCols);
 
         for (const col in route) {
-          const routeCol = document.createElement('li');
+          const routeCol = document.createElement("li");
           const elem = `${col}: ${route[col]}`;
           routeCol.append(elem);
           routeCols.append(routeCol);
-        } routesTable.append(routeElement);
-      } app.append(routesTable);
+        }
+        routesTable.append(routeElement);
+      }
+      app.append(routesTable);
     });
 }
 
@@ -92,49 +98,68 @@ function getOutgoingRoutes() {
   const originQuery = `origin=${origin}`;
 
   fetch(`/api/outgoingRoutes?${originQuery}`)
-    .then(res => res.json())
+    .then((res) => res.json())
     .then((res) => {
       const app = document.getElementById("station");
-      app.innerHTML = '';
+      app.innerHTML = "";
 
-      const routesTable = document.createElement('ul');
+      const routesTable = document.createElement("ul");
       for (const route of res) {
-        const routeElement = document.createElement('li');
-        const routeCols = document.createElement('ul');
+        const routeElement = document.createElement("li");
+        const routeCols = document.createElement("ul");
         routeElement.append(routeCols);
 
         for (const col in route) {
-          const routeCol = document.createElement('li');
+          const routeCol = document.createElement("li");
           const elem = `${col}: ${route[col]}`;
           routeCol.append(elem);
           routeCols.append(routeCol);
-        } routesTable.append(routeElement);
-      } app.append(routesTable);
+        }
+        routesTable.append(routeElement);
+      }
+      app.append(routesTable);
     });
 }
 
-function getPassengerTickets(){
-  const passengerName = document.getElementById('passenger-name').value;
+function getPassengerTickets() {
+  const passengerName = document.getElementById("passenger-name").value;
   const passengerNameQuery = `passenger=${passengerName}`;
-  console.log(passengerNameQuery)
+  console.log(passengerNameQuery);
   fetch(`/api/tickets?${passengerNameQuery}`)
-    .then(res => res.json())
+    .then((res) => res.json())
     .then((res) => {
-      const app = document.getElementById("tickets")
-      app.innerHTML = ''
-      
-      const ticketsTable = document.createElement('ul');
-      for (const ticket of res){
-        const ticketRow = document.createElement('li');
-        const ticketCols = document.createElement('ul');
+      const app = document.getElementById("tickets");
+      app.innerHTML = "";
+
+      const ticketsTable = document.createElement("ul");
+      for (const ticket of res) {
+        const ticketRow = document.createElement("li");
+        const ticketCols = document.createElement("ul");
         ticketRow.append(ticketCols);
-        
-        for (const col in ticket){
-          const ticketCol = document.createElement('li');
+
+        for (const col in ticket) {
+          const ticketCol = document.createElement("li");
           const elem = `${col}: ${ticket[col]}`;
-          ticketCol.append(elem)
+          ticketCol.append(elem);
           ticketCols.append(ticketCol);
-        } ticketsTable.append(ticketRow);
-      } app.append(ticketsTable);
-    })
+        }
+        ticketsTable.append(ticketRow);
+      }
+      app.append(ticketsTable);
+    });
 }
+
+function postMaintenanceLog() {
+  const form = document.getElementById("maintenance-log-post");
+  const fd = new FormData(form);
+  const urlEncoded = new URLSearchParams(fd).toString();
+
+  fetch("/api/logs", {
+    method: "POST",
+    body: urlEncoded,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  }).then((res) => console.log(res));
+}
+
